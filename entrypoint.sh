f613cbd3299b30bbd9394e60da1e8d24a403ff86
Aleksandr Fofanov <avl.fofanov@gmail.com>
2019-10-28 20:58:52 +0700

    Fix entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 EC2_INTERNAL_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/local-hostname)
 EC2_PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 
-if [[ ! -z ${EC2_PUBLIC_HOSTNAME} ]]; then
+if [ ! -z ${EC2_PUBLIC_HOSTNAME} ]; then
     export EC2_HOSTNAME=${EC2_PUBLIC_HOSTNAME}
 else
     export EC2_HOSTNAME=${EC2_INTERNAL_HOSTNAME}
