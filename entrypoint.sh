bbf37518343daeb279f7eff917432e67b4c9f949
Aleksandr Fofanov <avl.fofanov@gmail.com>
2019-10-29 20:25:35 +0700

    Switch to ec2 local hostname

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-EC2_INTERNAL_HOSTNAME=$(curl --silent http://169.254.169.254/latest/meta-data/local-hostname)
-EC2_PUBLIC_HOSTNAME=$(curl --silent http://169.254.169.254/latest/meta-data/public-hostname)
-
-if [[ ! -z ${EC2_PUBLIC_HOSTNAME} ]]; then
-    export EC2_HOSTNAME=${EC2_PUBLIC_HOSTNAME}
-else
-    export EC2_HOSTNAME=${EC2_INTERNAL_HOSTNAME}
-fi
+export EC2_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)"
 
 exec env fluentd -c /fluentd/etc/${FLUENTD_CONF} --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
