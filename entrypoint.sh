b497567f4e7d11499b13ce0e0fb7d137fbcd9e1a
Aleksandr Fofanov <avl.fofanov@gmail.com>
2020-01-24 17:04:48 +0700

    Add environment variables that allow to configure intake parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-export EC2_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)"
+export DD_INTAKE_HOST=${DD_INTAKE_HOST:-"intake.logs.datadoghq.com"}
+export DD_INTAKE_PORT=${DD_INTAKE_PORT:-10514}
+export DD_INTAKE_SSL_PORT=${DD_INTAKE_SSL_PORT:-10516}
+export DD_USE_SSL=${DD_USE_SSL:-true}
+export DD_HOSTNAME="$(curl -s http://169.254.169.254/latest/meta-data/local-hostname || hostname -f)"
 
 exec env fluentd -c /fluentd/etc/${FLUENTD_CONF} --gemfile /fluentd/Gemfile ${FLUENTD_OPT}
